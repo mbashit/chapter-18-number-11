3de07cea121256f8a350f0b234523413090e8833
Bashitapwa Michael <michaelbashitapwa52@gmail.com>
2022-10-08 01:14:53 +0300

    Add project files.

---
+++ chapter 18 number 11/chapter 18 number 11.cpp
@@ -0,0 +1,48 @@
+﻿// chapter 18 number 11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*11.
+
+The most popular and commonly used grading system in the United
+States uses discrete evaluation in the form of letter grades. Design a
+flowchart and write the corresponding C++ program that prompts the
+user to enter a letter between A and F, and then displays the
+corresponding percentage according to the following table.
+Grade Percentage
+A 90 ‑ 100
+B 80 ‑ 89
+C 70 ‑ 79
+D 60 ‑ 69
+E / F 0 ‑ 59
+Assume that the user enters a valid value.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double A, B, C, D, E, F;
+
+	cout << "enter a letter (A-F): ";
+	cin >> A >> B >> C >> D >> E >> F;
+
+	if (A >=90 && A <=100) {
+		cout << "Grade is (90 - 100)";
+	}
+	else if (B >=80 && B <= 89) {
+		cout << "Grade is (80-89)";
+	}
+	else if (C >=70 && C <= 79) {
+		cout << " Grade is (70 -79)";
+	}
+	else if (D >= 60 && D <= 69) {
+		cout << "Grade is (60 -69)";
+	}
+	else if (E >=0 && E <=59 || F >= 0 && E <= 59) {
+		cout << "Grade is (0 -59)";
+	}
+	else {
+		cout << "invalid input" << endl;
+	}
+	return 0;
+}
+
